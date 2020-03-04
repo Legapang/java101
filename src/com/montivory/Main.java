62c6de5ce9aecf6c36f17f9192f8788c3b9f36b7
Legapang <59070089@it.kmitl.ac.th>
2020-03-04 16:45:45 +0700

    Update Main

--- src/com/montivory/Main.java
+++ src/com/montivory/Main.java
@@ -3,6 +3,6 @@ package com.montivory;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Hello, world of Git");
     }
 }
